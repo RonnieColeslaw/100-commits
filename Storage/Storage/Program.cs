060ca08f572147710fdfbe0f561230976dc16e9f
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-03-01 19:15:12 +0100

    1/100 First commit

--- Storage/Storage/Program.cs
+++ Storage/Storage/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LegoStorage;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Witaj w magazynie! Co chcesz zrobić?");
+        Console.WriteLine("1.Dodaj zestaw");
+        Console.WriteLine("2.Usuń zestaw");
+        Console.WriteLine("3.Znajdź zestaw");
+        Console.WriteLine("4.Wyświetl wszystkie zestawy");
+        Console.WriteLine("X.Zamknij program");
+
+        var userChoice = Console.ReadLine();
+    }
+}
