b7d2e6bd053cb5cccad9459fe072fd624a86884f
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-03-02 20:48:39 +0100

    2/100 Added class LegoList

---
+++ Storage/Storage/Model/LegoSet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Model
+{
+    public class LegoSet
+    {
+
+        public LegoSet(string setName, string setNumber, string series, int elementsQuantity, DateTime releaseDate, decimal retailPrice, decimal resellPrice, string warehouse)
+        {
+            SetName = setName;
+            SetNumber = setNumber;
+            Series = series;
+            ElementsQuantity = elementsQuantity;
+            ReleaseDate = releaseDate;
+            RetailPrice = retailPrice;
+        
+        }
+
+        public string SetName { get; set; }
+
+        public string SetNumber { get; set; }
+
+        public string Series { get; set; }
+
+        public int ElementsQuantity { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal RetailPrice { get; set; }
+   
+    }
+}
---
+++ Storage/Storage/Services/LegoCatalog.cs
@@ -0,0 +1,14 @@
+﻿using Storage.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Services
+{
+    class LegoCatalog
+    {
+        private List<LegoSet> LegoSetsList { get; set; } = new List<LegoSet>();
+    }
+}
