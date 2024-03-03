8d97abcf25274c0a8acdd16ef44cea98d8201491
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-03-03 21:33:38 +0100

    3/100 Added user choice

--- Storage/Storage/Program.cs
+++ Storage/Storage/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LegoStorage;
+﻿using Storage.Model;
+
+namespace LegoStorage;
 
 class Program
 {
@@ -12,5 +14,59 @@ class Program
         Console.WriteLine("X.Zamknij program");
 
         var userChoice = Console.ReadLine();
+
+        while (true)
+        {
+            switch (userChoice)
+            {
+                case "1":
+                    Console.WriteLine("Podaj NAZWĘ zestawu:");
+                    var input_Name = Console.ReadLine();
+                    Console.WriteLine("Podaj SERIĘ zestawu:");
+                    var input_Series = Console.ReadLine();
+                    Console.WriteLine("Podaj NUMER zestawu:");
+                    var input_Number = Console.ReadLine();
+                    Console.WriteLine("Podaj ILOŚĆ ELEMENTÓW zestawu:");
+                    var input_PartQuantity = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj DATĘ WYDANIA zestawu (yyyy/mm/dd):");
+                    var input_ReleaseDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj CENĘ SKLEPOWĄ zestawu (PLN):");
+                    var input_RetailPrice = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine("Podaj CENĘ RYNKOWA zestawu (PLN):");
+                    var input_ResellPrice = decimal.Parse(Console.ReadLine());
+                    var warehouse = string.Empty;
+
+                    var newSet = new LegoSet(input_Name, input_Number, input_Series, input_PartQuantity, input_ReleaseDate, input_RetailPrice, input_ResellPrice, warehouse);
+
+               
+
+                    break;
+                case "2":
+                    Console.WriteLine("Podaj nazwę lub numer zestawu do usunięcia");
+                    
+
+                    break;
+                case "3":
+                    Console.WriteLine("Podaj szukany numer zestawu:");
+                   
+
+                    break;
+
+                case "4":
+                    Console.WriteLine("Wyświetl wszystkie zestawy");
+                  
+                    break;
+
+                case "x":
+                case "X":
+                    return;
+                default:
+                    Console.WriteLine($"Brak opcji: {userChoice}");
+                    break;
+
+            }
+            Console.WriteLine("Co chcesz zrobić?");
+            userChoice = Console.ReadLine();
+        }
     }
 }
