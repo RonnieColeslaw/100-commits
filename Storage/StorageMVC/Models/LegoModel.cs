dea4b3a771624a5277caf6350d47b45daba9358f
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-03-09 20:54:58 +0100

    9/100 Creating MVC schema + changes in Layout +adding Model

---
+++ Storage/StorageMVC/Models/LegoModel.cs
@@ -0,0 +1,23 @@
+﻿namespace LegoMVC.Models
+{
+    public class LegoModel
+    {
+        public string SetName { get; set; }
+
+        public string SetNumber { get; set; }
+
+        public string Series { get; set; }
+
+        public int ElementsQuantity { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal RetailPrice { get; set; }
+        public decimal ResellPrice { get; set; }
+
+        public string Warehouse { get; set; }
+
+      
+
+    }
+}
