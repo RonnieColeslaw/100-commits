f70d229e262c7ea3f076dee781ac0844eecca6d6
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-04-10 21:52:56 +0200

    41/100 Background photo debug session

--- Storage/StorageMVC/Content/Photos/Photo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StorageMVC.Content.Photos;
-
-public class Photo
-{
-    public int Id { get; set; }
-    public string FileName { get; set; }
-    public string FilePath { get; set; }
-
-}
