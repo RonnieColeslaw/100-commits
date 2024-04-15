1e0adbd3f9f5fa428777b8914d3c762ff848972a
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-04-15 22:40:25 +0200

    46/100 Added Search controller

---
+++ Storage/StorageMVC/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using LegoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorageMVC.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly SharedServices _sharedService;
+
+        public SearchController(SharedServices sharedService)
+        {
+            _sharedService = sharedService;
+        }
+
+        public ActionResult Search(string input)
+        {
+            List<LegoModel> legoSets = _sharedService.GetLegoSets();
+            LegoModel legoSetToFind = legoSets.FirstOrDefault(s => s.SetName.ToLower().Contains(input.ToLower()));
+
+            return View("SearchResults", legoSetToFind);
+        }
+    }
+}
+z
