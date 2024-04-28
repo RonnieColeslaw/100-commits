7c27ee76a3ece76901e94cfb76874b797239322f
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-04-28 21:39:26 +0200

    59/100 Added EF to Edit controller

--- Storage/StorageMVC/Controllers/EditController.cs
+++ Storage/StorageMVC/Controllers/EditController.cs
@@ -8,36 +8,73 @@ namespace StorageMVC.Controllers;
 
 public class EditController : Controller
 {
+    private readonly LegoDbContext _context;
 
-    private readonly SharedServices _sharedService;
-
-    public EditController(SharedServices sharedService)
+    public EditController(LegoDbContext context)
     {
-        _sharedService = sharedService;
+        _context = context;
     }
 
-    public ActionResult Edit(int id)
+    public IActionResult Edit(int id)
     {
-        List<LegoModel> legoSets = _sharedService.GetLegoSets();
-        LegoModel legoSetToEdit = legoSets.FirstOrDefault(s => s.SetNumber == id.ToString());
+        LegoModel legoSetToEdit = _context.LegoModels.FirstOrDefault(s => s.SetNumber == id.ToString());
 
+        if (legoSetToEdit == null)
+        {
+            return NotFound();
+        }
 
         legoSetToEdit.SeriesList = new List<SelectListItem>
-    {
-        new SelectListItem { Value = "Series3", Text = "LEGO® Architecture" },
-        new SelectListItem { Value = "Series1", Text = "LEGO® Art" },
+            {
+                new SelectListItem { Value = "Series3", Text = "LEGO® Architecture" },
+                new SelectListItem { Value = "Series1", Text = "LEGO® Art" },
 
-    };
+            };
 
 
         legoSetToEdit.WarehousesList = new List<SelectListItem>
-    {
-        new SelectListItem {Value = "Warehouse 1", Text = "Warehouse 1"},
-        new SelectListItem {Value = "Warehouse 2", Text = "Warehouse 2"},
+            {
+                new SelectListItem {Value = "Warehouse 1", Text = "Warehouse 1"},
+                new SelectListItem {Value = "Warehouse 2", Text = "Warehouse 2"},
 
-    };
+            };
 
         return View("~/Views/Lego/EditLego.cshtml", legoSetToEdit);
     }
+}
+
+//public class EditController : Controller
+//{
+
+//    private readonly SharedServices _sharedService;
+
+//    public EditController(SharedServices sharedService)
+//    {
+//        _sharedService = sharedService;
+//    }
+
+//    public ActionResult Edit(int id)
+//    {
+//        List<LegoModel> legoSets = _sharedService.GetLegoSets();
+//        LegoModel legoSetToEdit = legoSets.FirstOrDefault(s => s.SetNumber == id.ToString());
+
+
+//        legoSetToEdit.SeriesList = new List<SelectListItem>
+//    {
+//        new SelectListItem { Value = "Series3", Text = "LEGO® Architecture" },
+//        new SelectListItem { Value = "Series1", Text = "LEGO® Art" },
+
+//    };
+
+
+//        legoSetToEdit.WarehousesList = new List<SelectListItem>
+//    {
+//        new SelectListItem {Value = "Warehouse 1", Text = "Warehouse 1"},
+//        new SelectListItem {Value = "Warehouse 2", Text = "Warehouse 2"},
+
+//    };
+
+//        return View("~/Views/Lego/EditLego.cshtml", legoSetToEdit);
+//    }
 
 }
