0211d4494c2580239b47aeaf955b8291fc447c11
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-05-02 20:55:41 +0200

    63/100 Added Db class

---
+++ Storage/StorageMVC/Models/Serie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Model;
+
+public class Serie
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+
+}
+
