5a0d61409578bd81a5a94df1651abebf88603e18
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-05-05 21:49:01 +0200

    66/100  DBContext changes

--- Storage/StorageMVC/Models/LegoSet.cs
+++ Storage/StorageMVC/Models/LegoSet.cs
@@ -1,6 +1,12 @@
-﻿namespace StorageMVC.Models
+﻿public class LegoSet
 {
-    public class Class
-    {
-    }
+    public int Id { get; set; }
+    public string SetName { get; set; }
+    public string SetNumber { get; set; }
+    public int? ElementsQuantity { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public double? RetailPrice { get; set; }
+    public double? ResellPrice { get; set; }
+    public string Warehouse { get; set; }
+    public string Series { get; set; }
 }
