1948f258276b1ddb6cb38b531e87cc9565792d05
Dominik Misiak <Dominik.Misiak@keikostudio.com>
2024-05-08 21:17:23 +0200

    69/100 Context

---
+++ Storage/StorageMVC/Models/LegoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class LegoContext : DbContext
+{
+    public DbSet<LegoSet> LegoStorage { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LegoSet>().ToTable("LegoStorage");
+    }
+}
